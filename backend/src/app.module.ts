1c504d4b5a34d1abf707196d5f9caf858c5aaf2d
matsumoto <dsongben@gmail.com>
2024-12-08 09:54:09 +0900

    moduleファイル作成

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TodoController } from './todo/todo.controller';
 import { TodoService } from './todo/todo.service';
+import { TodoModule } from './todo/todo.module';
 
 @Module({
-  imports: [],
+  imports: [TodoModule],
   controllers: [AppController, TodoController],
   providers: [AppService, TodoService],
 })
