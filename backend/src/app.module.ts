9334fc3e26b31a2507e052df91cb50a079802004
matsumoto <dsongben@gmail.com>
2024-12-08 09:53:44 +0900

    serviceファイル作成

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TodoController } from './todo/todo.controller';
+import { TodoService } from './todo/todo.service';
 
 @Module({
   imports: [],
   controllers: [AppController, TodoController],
-  providers: [AppService],
+  providers: [AppService, TodoService],
 })
 export class AppModule {}
