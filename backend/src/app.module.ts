bec43b12df426927fa922ff451b4fcc5ed8289e3
matsumoto <dsongben@gmail.com>
2024-12-17 21:53:45 +0900

    メインモジュールでuserを呼び出し/getできていることを確認

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,10 +5,13 @@ import { TodoController } from './todo/todo.controller'
 import { TodoService } from './todo/todo.service'
 import { TodoModule } from './todo/todo.module'
 import { ConfigModule } from '@nestjs/config'
+import { UserService } from './user/user.service'
+import { UserController } from './user/user.controller'
+import { PrismaService } from './prisma/prisma.service'
 
 @Module({
   imports: [ConfigModule.forRoot(), TodoModule],
-  controllers: [AppController, TodoController],
-  providers: [AppService, TodoService],
+  controllers: [AppController, TodoController, UserController],
+  providers: [AppService, TodoService, UserService, PrismaService],
 })
 export class AppModule {}
