416dda84bebba1d60e8e8e8584cb069cd1d9b0dc
matsumoto <dsongben@gmail.com>
2024-12-21 05:45:54 +0900

    CRUD処理のAPI実装

--- backend/src/todo/todo.controller.ts
+++ backend/src/todo/todo.controller.ts
@@ -1,4 +1,30 @@
-import { Controller } from '@nestjs/common'
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common'
+import { TodoService } from './todo.service'
+import { Prisma, Todo } from '@prisma/client'
 
 @Controller('todo')
-export class TodoController {}
+export class TodoController {
+  constructor(private readonly todoService: TodoService) {}
+
+  @Get()
+  async getTodos(@Body() data: Prisma.TodoWhereInput): Promise<Todo[]> {
+    return this.todoService.todos(data)
+  }
+
+  @Post()
+  async createTodo(@Body() data: Prisma.TodoCreateInput): Promise<Todo> {
+    return this.todoService.createTodo(data)
+  }
+
+  @Put(':id')
+  async doneTodo(@Param('id') id: number): Promise<Todo> {
+    return this.todoService.doneTodo({
+      where: { id },
+    })
+  }
+
+  @Delete(':id')
+  async deleteTodo(@Param('id') id: number): Promise<Todo> {
+    return this.todoService.deleteTodo({ id })
+  }
+}
--- backend/src/todo/todo.service.ts
+++ backend/src/todo/todo.service.ts
@@ -1,4 +1,41 @@
 import { Injectable } from '@nestjs/common'
+import { Prisma, Todo } from '@prisma/client'
+import { PrismaService } from 'src/prisma/prisma.service'
 
 @Injectable()
-export class TodoService {}
+export class TodoService {
+  constructor(private prisma: PrismaService) {}
+
+  async todos(data: Prisma.TodoWhereInput): Promise<Todo[]> {
+    return this.prisma.todo.findMany({
+      where: {
+        userId: data.userId,
+      },
+    })
+  }
+
+  async createTodo(data: Prisma.TodoCreateInput): Promise<Todo> {
+    return this.prisma.todo.create({
+      data,
+    })
+  }
+
+  async doneTodo(data: { where: Prisma.TodoWhereUniqueInput }): Promise<Todo> {
+    return this.prisma.todo.update({
+      where: {
+        id: Number(data.where.id),
+      },
+      data: {
+        done: true,
+      },
+    })
+  }
+
+  async deleteTodo(where: Prisma.TodoWhereUniqueInput): Promise<Todo> {
+    return this.prisma.todo.delete({
+      where: {
+        id: Number(where.id),
+      },
+    })
+  }
+}
