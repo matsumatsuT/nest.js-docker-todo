9d679ec210fdd2009fdd519657512850b4cb3d18
matsumoto <dsongben@gmail.com>
2024-12-29 15:02:11 +0900

    ValidationPipeにオプションを追加

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,16 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+import { ValidationPipe } from '@nestjs/common'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, //不要なDTOフィールドを削除
+      forbidNonWhitelisted: true, //不要なDTOフィールドがある場合はエラーを返す
+    }),
+  )
 
   const config = new DocumentBuilder()
     .setTitle('Todo API')
