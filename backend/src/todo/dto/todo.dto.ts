6cd319d39b2a7bf3ea010c9b848a84e8b3e35aab
matsumoto <dsongben@gmail.com>
2024-12-29 15:01:55 +0900

    todoAPIをswaggerで表示する

--- backend/src/todo/dto/todo.dto.ts
+++ backend/src/todo/dto/todo.dto.ts
@@ -1,27 +1,40 @@
-import { IsNumber, IsOptional, IsString, MinLength } from 'class-validator'
+import { ApiProperty } from '@nestjs/swagger'
+import {
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsString,
+  MinLength,
+} from 'class-validator'
 
 export class CreateTodoDto {
+  @ApiProperty()
   @IsString()
-  @MinLength(1)
+  @MinLength(1, { message: 'タイトルは入力必須です' })
+  @IsNotEmpty()
   title: string
 
+  @ApiProperty({ required: false, example: '説明はオプショナルです' })
   @IsString()
   @IsOptional()
   description?: string
 
   // サーバー側でidをセットしたい
   // @Requestで取得できそう。認証機能が必要なので気が向いたらやる
+  @ApiProperty()
   @IsNumber()
   userId: number
 }
 
 // Doneにするためのもの
 export class UpdateTodoDto {
+  @ApiProperty()
   @IsNumber()
   id: number
 }
 
 export class DeleteTodoDto {
+  @ApiProperty()
   @IsNumber()
   id: number
 }
