dd1b560b7192b27dd5879d35bbea97fd2595e352
matsumoto <dsongben@gmail.com>
2025-01-02 12:54:21 +0900

    API呼び出し。値は取得できるが型情報が入っていない

--- frontend/src/app/list/@user/page.tsx
+++ frontend/src/app/list/@user/page.tsx
@@ -1,5 +1,15 @@
+"use client";
+
+import { useFetchClient } from "@/hooks/useFetchClient";
+
 const User = () => {
-  return <div>ユーザー</div>;
+  const $api = useFetchClient();
+
+  // 取得できるがdataの型情報が取得できない
+  // DTOかymlでレスポンスの方を敵意義する必要があるかも
+  const { data, isLoading, error } = $api.useQuery("get", "/user");
+
+  return <div>{data}</div>;
 };
 
 export default User;
