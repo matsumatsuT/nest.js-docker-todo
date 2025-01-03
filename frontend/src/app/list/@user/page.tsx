f74ff4f7bfb1af7b1b276dbca24277dd180cab40
matsumoto <dsongben@gmail.com>
2025-01-03 15:53:19 +0900

    dataの型が変更されている事を確認

--- frontend/src/app/list/@user/page.tsx
+++ frontend/src/app/list/@user/page.tsx
@@ -5,11 +5,17 @@ import { useFetchClient } from "@/hooks/useFetchClient";
 const User = () => {
   const $api = useFetchClient();
 
-  // 取得できるがdataの型情報が取得できない
-  // DTOかymlでレスポンスの方を敵意義する必要があるかも
   const { data, isLoading, error } = $api.useQuery("get", "/user");
 
-  return <div>{data}</div>;
+  if (!data) return null;
+
+  return (
+    <div>
+      {data.map((user) => (
+        <div key={user.id}>{user.name}</div>
+      ))}
+    </div>
+  );
 };
 
 export default User;
