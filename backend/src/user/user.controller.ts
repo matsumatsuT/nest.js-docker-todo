f7b266458249de6b1717bdcc8594bc2ecdb6e602
matsumoto <dsongben@gmail.com>
2025-01-03 15:52:12 +0900

    UserResponseDtoをControllerで使用/swaggerの更新

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -1,13 +1,15 @@
 import { Body, Controller, Get, Post } from '@nestjs/common'
 import { UserService } from './user.service'
 import { User } from '@prisma/client'
-import { CreateUserDto } from './dto/user.dto'
+import { CreateUserDto, UserResponseDto } from './dto/user.dto'
+import { ApiOkResponse } from '@nestjs/swagger'
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Get()
+  @ApiOkResponse({ type: UserResponseDto, isArray: true })
   async getUsers(): Promise<User[]> {
     return this.userService.users()
   }
