72fbb7bed62e0aff4b5aaf90831ac7524d856b0a
matsumoto <dsongben@gmail.com>
2025-01-06 07:19:57 +0900

    schemaに合わせてusersに変更

--- frontend/src/app/list/@user/page.tsx
+++ frontend/src/app/list/@user/page.tsx
@@ -1,18 +1,16 @@
 "use client";
 
-import { useFetchClient } from "@/hooks/useFetchClient";
+import { $api } from "@/api/client";
 import { CreateUser, FormValues } from "./CreateUser";
 
 const User = () => {
-  const $api = useFetchClient();
-
-  const { data, refetch } = $api.useQuery("get", "/user");
-  const { mutate } = $api.useMutation("delete", "/user/{id}", {
+  const { data, refetch } = $api.useQuery("get", "/users");
+  const { mutate } = $api.useMutation("delete", "/users/{id}", {
     onSuccess: () => {
       refetch();
     },
   });
-  const { mutate: createUser } = $api.useMutation("post", "/user", {
+  const { mutate: createUser } = $api.useMutation("post", "/users", {
     onSuccess: () => {
       refetch();
     },
