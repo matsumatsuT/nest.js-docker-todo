e89c3be4c0c0927876ba2531a43284a382589933
matsumoto <dsongben@gmail.com>
2025-01-18 13:26:11 +0900

    signInでaccess_tokenを返すように修正

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,19 +1,25 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common'
+import { JwtService } from '@nestjs/jwt'
 import { UserService } from 'src/user/user.service'
 
 @Injectable()
 export class AuthService {
-  constructor(private userService: UserService) {}
+  constructor(
+    private userService: UserService,
+    private jwtService: JwtService,
+  ) {}
 
   async signIn(email: string, pass: string) {
     const user = await this.userService.user({ email: email })
+
     if (user.password !== pass) {
       throw new UnauthorizedException('Invalid email or password')
     }
 
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const { password, ...result } = user // passwordは返さない
+    const payload = { sub: user.id, email: user.name }
 
-    return result
+    return {
+      access_token: await this.jwtService.signAsync(payload),
+    }
   }
 }
