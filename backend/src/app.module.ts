0ec9e4f5804612b504eb2aeae96324b45610cbdd
matsumoto <dsongben@gmail.com>
2025-01-18 13:30:44 +0900

    appModuleで使用するmoduleを呼び出し

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -8,9 +8,11 @@ import { ConfigModule } from '@nestjs/config'
 import { UserService } from './user/user.service'
 import { UserController } from './user/user.controller'
 import { PrismaService } from './prisma/prisma.service'
+import { AuthModule } from './auth/auth.module'
+import { UserModule } from './user/user.module'
 
 @Module({
-  imports: [ConfigModule.forRoot(), TodoModule],
+  imports: [ConfigModule.forRoot(), TodoModule, UserModule, AuthModule],
   controllers: [AppController, TodoController, UserController],
   providers: [AppService, TodoService, UserService, PrismaService],
 })
