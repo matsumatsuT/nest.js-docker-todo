78b7840d87ff9ed75da46920e9c0e0de1e876eb6
matsumoto <dsongben@gmail.com>
2025-01-22 23:51:18 +0900

    中身を空にする

--- frontend/src/app/list/page.tsx
+++ frontend/src/app/list/page.tsx
@@ -1,14 +1,3 @@
-"use client";
-
-import { useRouter } from "next/navigation";
-
 export default function Home() {
-  const router = useRouter();
-
-  return (
-    <div>
-      <button onClick={() => router.push("/user")}>ユーザー</button>
-      <button onClick={() => router.push("/todo")}>TODO</button>
-    </div>
-  );
+  return <></>;
 }
