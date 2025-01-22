6a20992ce88218f38b8551ac6e32a0861ed8cb92
matsumoto <dsongben@gmail.com>
2025-01-22 23:55:56 +0900

    middlewareないでAuthorizationにセットする処理を追加

--- frontend/src/api/client.ts
+++ frontend/src/api/client.ts
@@ -1,9 +1,25 @@
-import createFetchClient from "openapi-fetch";
+import createFetchClient, { Middleware } from "openapi-fetch";
 import { paths } from "./schema";
 import createClient from "openapi-react-query";
 
+import Cookies from "js-cookie";
+
+const middleware: Middleware = {
+  onRequest: async ({ request }) => {
+    const access_token = Cookies.get("access_token");
+    if (access_token) {
+      request.headers.set("Authorization", `Bearer ${access_token}`);
+    }
+  },
+};
+
 const fetchClient = createFetchClient<paths>({
   baseUrl: "http://localhost:3308/",
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
+
+fetchClient.use(middleware);
 
 export const $api = createClient(fetchClient);
