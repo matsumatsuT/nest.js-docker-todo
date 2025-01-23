d913f1f15708a7ddbd54155fd490f7bb2abb111a
matsumoto <dsongben@gmail.com>
2025-01-23 23:53:39 +0900

    wip:認証時間を増やして対応

--- frontend/src/api/client.ts
+++ frontend/src/api/client.ts
@@ -4,12 +4,21 @@ import createClient from "openapi-react-query";
 
 import Cookies from "js-cookie";
 
+// TODO: 時間が経つとAPIが叩けなくなるのでリフレッシュトークンの処理を追加する
 const middleware: Middleware = {
   onRequest: async ({ request }) => {
     const access_token = Cookies.get("access_token");
     if (access_token) {
       request.headers.set("Authorization", `Bearer ${access_token}`);
     }
+  },
+  onResponse: async ({ response }) => {
+    // 認可エラー発生時にログイン画面へ遷移
+    if (response.status === 401) {
+      console.log("認証エラー発火");
+      window.location.href = "/login";
+    }
+    return response;
   },
 };
 
